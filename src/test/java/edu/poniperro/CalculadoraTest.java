1cad8c9247a3931da631cb71bda7a872d5d9baf8
jouad01 <jouardiouardi@cifpfbmoll.eu>
2022-05-10 19:20:46 +0200

    test: Creados tests unitarios;\

---
+++ src/test/java/edu/poniperro/CalculadoraTest.java
@@ -0,0 +1,18 @@
+package edu.poniperro;
+
+import junit.framework.TestCase;
+
+public class CalculadoraTest extends TestCase {
+
+    public void testSuma() {
+    }
+
+    public void testResta() {
+    }
+
+    public void testDivide() {
+    }
+
+    public void testMultiplica() {
+    }
+}
